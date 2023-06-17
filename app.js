4f12734507f815503edd4512d98dd26ec3990091
Abheekrishna <abheekrishna17@gmail.com>
2023-06-17 12:01:26 +0530

    Finished styling

---
+++ app.js
@@ -0,0 +1,26 @@
+const numberBtns = document.querySelectorAll('[data-number]');
+const operationBtns = document.querySelectorAll('[data-operation]');
+const equalBtn = document.querySelector('[data-equals]');
+const allClearBtn = document.querySelector('[data-all-clear]');
+const deleteBtn = document.querySelector('[data-delete]');
+
+
+const add = (a, b) => {
+    return parseFloat(a) + parseFloat(b);
+}
+
+const subtract = (a, b) => {
+    return parseFloat(a) - parseFloat(b);
+}
+
+const multiply = (a, b) => {
+    return parseFloat(a) * parseFloat(b);
+}
+
+const divide = (a, b) => {
+    if(a === 0 || b === 0) {
+        return;
+    } else {
+        return parseFloat(a) / parseFloat(b);
+    }
+}
